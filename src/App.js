63d573eb71d58f979a17bcf8882ca9073862e991
magicean <55060483+magicean@users.noreply.github.com>
2022-11-24 19:33:07 -0500

    changed footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import Search from "./Search";
 import Header from "./Header";
-import Footer from "./Footer";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
         <Search />
         <Header />
       </div>
-      <Footer />
     </div>
   );
 }
